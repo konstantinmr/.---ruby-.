ba4faf94308f12de1951c4327a4f0b51c8ae3144
kmironov <illgl9@mail.ru>
2020-11-24 17:03:11 +0300

    new files

---
+++ coin.rb
@@ -0,0 +1,5 @@
+if rand(2) == 1
+	puts "Выпал орел"
+else
+	puts "Выпала решка"
+end
---
+++ coin_ex.rb
@@ -0,0 +1,10 @@
+if rand(10) == 1
+	puts "Монета встала на ребро"
+else 
+
+if rand(2) == 1
+	puts "Выпал орел"
+else 
+	puts "Выпала решка"
+end
+end
